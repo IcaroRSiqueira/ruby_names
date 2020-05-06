e76d099043e0905769828c921f30b2a7b55d2075
IcaroRSiqueira <icarorob@hotmail.com>
2020-05-06 19:49:50 -0300

    Realizando input atraves do rspec

--- lib/ruby_names.rb
+++ lib/ruby_names.rb
@@ -2,7 +2,7 @@ class RubyNames
   def self.welcome
     puts 'Bem vindo ao RubyNames!'
     puts 'Digite o UF para saber quais são os nomes mais comuns no estado'
-    input = gets.chomp
+    input = $stdin.gets.chomp
     puts "O estado escolhido é #{input}"
   end
 end
--- spec/lib/ruby_names_spec.rb
+++ spec/lib/ruby_names_spec.rb
@@ -4,13 +4,9 @@ require 'ruby_names'
 describe "ruby_names" do
 
   it "displays welcome message and uf selected" do
-    printed = capture_stdout do
-      RubyNames.welcome
-    end
-
-    expect($stdout).to include("Bem vindo ao RubyNames!\n")
-    expect(printed).to include("Digite o UF para saber quais são os nomes mais comuns no estado\n")
     allow($stdin).to receive(:gets).and_return("SP\n")
-    expect(printed).to include("O estado escolhido é SP\n")
+
+    expect { load "./lib/run.rb" }.to output("Bem vindo ao RubyNames!\nDigite o UF para saber quais são os nomes mais comuns no estado\nO estado escolhido é SP\n").to_stdout
+
   end
 end
